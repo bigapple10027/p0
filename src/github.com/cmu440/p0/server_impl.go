29710e1a1e7343dd6fd2a6f2c06be3700e26800f
Hao Hu <haohu321@gmail.com>
2017-10-31 09:22:51 -0700

    Commit initial changes

--- src/github.com/cmu440/p0/server_impl.go
+++ src/github.com/cmu440/p0/server_impl.go
@@ -2,18 +2,32 @@
 
 package p0
 
+import (
+	"fmt"
+	"log"
+)
 type keyValueServer struct {
     // TODO: implement this!
+	connectionChannel chan Conn
 }
 
 // New creates and returns (but does not start) a new KeyValueServer.
 func New() KeyValueServer {
-    // TODO: implement this!
-    return nil
+	var ret = keyValueServer{}
+	// TODO: initialization here
+	return ret
 }
 
 func (kvs *keyValueServer) Start(port int) error {
-    // TODO: implement this!
+    // TODO: go handler that handles all the request with channels.
+	listener, err := net.Listen("tcp", ":" + iota(port))
+	if err != nil {
+		fmt.Println("keyValueServre.Start: Can't create a listener, error occured")
+		return err
+	}
+	kvs.listener = listener
+
+	go runServer(kvs)
     return nil
 }
 
@@ -27,3 +41,34 @@ func (kvs *keyValueServer) Count() int {
 }
 
 // TODO: add additional methods/functions below!
+
+func runServer(kvs *keyValueServer) {
+	for {
+		select {
+			// Handle when there is new connection.
+			case x < x:
+				// Add the client to kvs.
+				// 
+
+			// Handle when client wants to access the underlying database
+			case 
+
+			//  Handle when client tries to disconnect
+			case
+
+			// Handle when client 		
+		}
+	}
+}
+
+func listenToConnections(listener *Listener) {
+	for {
+		connection, err := listener.Accept()
+		if nil != err {
+			// handle err
+		} else {
+			// notify server handler thread that there is a new connection.
+			// Pass the new connection back through channel
+		}
+	}
+}

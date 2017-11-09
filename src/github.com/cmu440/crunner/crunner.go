087bd3cd19ec449c0087ba7fdf392d186d2d250f
Hao Hu <haohu321@gmail.com>
2017-11-09 07:27:47 -0800

    Adding the initial changes

--- src/github.com/cmu440/crunner/crunner.go
+++ src/github.com/cmu440/crunner/crunner.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"bufio"
     "fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 const (
@@ -15,5 +22,36 @@ const (
 // read and print out the server's response to standard output. Whether or
 // not you add any code to this file will not affect your grade.
 func main() {
-    fmt.Println("Not implemented.")
+	addr := strings.Join([]string{defaultHost, strconv.Itoa(defaultPort)}, ":")
+	conn, err := net.Dial("tcp", addr)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+
+	defer conn.Close()
+	timeoutDuration := 3 * time.Second
+	conn.SetReadDeadline(time.Now().Add(timeoutDuration))
+
+	buff := make([]byte, 1024)
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("Enter the message >> ")
+		text, _ := reader.ReadString('\n')
+		conn.Write([]byte(text))
+		log.Printf("Send: %s\n", text)
+		n, err := conn.Read(buff)
+		if nil != err {
+			fmt.Printf("Received: %s\n\n", buff[:n])
+		}
+	}
+	// scanner := bufio.NewScanner(os.Stdin)
+	// fmt.Printf("Enter the message >> ")
+	// for scanner.Scan() {
+	// 	conn.Write([]byte(scanner.Bytes()))
+	// 	fmt.Printf("Sent: %s\n", scanner.Text())
+	// 	fmt.Printf("Enter the message >> ")
+	// }
 }
--- src/github.com/cmu440/p0/server_impl.go
+++ src/github.com/cmu440/p0/server_impl.go
@@ -3,24 +3,64 @@
 package p0
 
 import (
+	"bufio"
 	"fmt"
-	"log"
+	//"log"
+	"io"
+	"net"
+	"strconv"
+	"strings"
 )
+
+const (
+	MAX_NUM_BYTES = 1024
+)
+
+type DBRequest struct {
+	isGet bool
+	key string
+	value string
+}
+
+type CountClientsRequest struct {
+	countChannel chan int
+}
+
+type Client struct {
+	connection net.Conn
+	bytesChannel chan []byte
+	quitReadChannel chan bool
+	quitWriteChannel chan bool
+}
+
 type keyValueServer struct {
     // TODO: implement this!
-	connectionChannel chan Conn
+	connectionChannel chan net.Conn
+	dbRequestChannel chan DBRequest
+	countClientsRequestChannel chan CountClientsRequest
+	deadClient chan Client
+	quitServer chan bool
+	quitListening chan bool
+	clients []Client
+	listener net.Listener
 }
 
 // New creates and returns (but does not start) a new KeyValueServer.
 func New() KeyValueServer {
-	var ret = keyValueServer{}
-	// TODO: initialization here
+	var ret = &keyValueServer{}
+	ret.connectionChannel = make(chan net.Conn)
+	ret.dbRequestChannel = make(chan DBRequest)
+	ret.countClientsRequestChannel = make(chan CountClientsRequest)
+	ret.quitListening = make(chan bool)
+	ret.quitServer = make(chan bool)
+	ret.clients = make([]Client, 0, 10)
+
 	return ret
 }
 
 func (kvs *keyValueServer) Start(port int) error {
     // TODO: go handler that handles all the request with channels.
-	listener, err := net.Listen("tcp", ":" + iota(port))
+	listener, err := net.Listen("tcp", ":" + strconv.Itoa(port))
 	if err != nil {
 		fmt.Println("keyValueServre.Start: Can't create a listener, error occured")
 		return err
@@ -32,43 +72,160 @@ func (kvs *keyValueServer) Start(port int) error {
 }
 
 func (kvs *keyValueServer) Close() {
-    // TODO: implement this!
+	fmt.Println("Close() got called")
+	kvs.listener.Close()
+	kvs.quitServer <- true
+	kvs.quitListening <- true
 }
 
 func (kvs *keyValueServer) Count() int {
-    // TODO: implement this!
-    return -1
+	countClientsRequest := CountClientsRequest{make(chan int)}
+	kvs.countClientsRequestChannel <- countClientsRequest
+    return <-countClientsRequest.countChannel
 }
+
+/*******************************************/
+// Private keyValueServer member functions
+/*******************************************/
+
+
+/********************************************/
+// End(Private keyValueSever member functions)
+/********************************************/
 
 // TODO: add additional methods/functions below!
-
 func runServer(kvs *keyValueServer) {
+	go listenToConnections(kvs)
 	for {
 		select {
-			// Handle when there is new connection.
-			case x < x:
-				// Add the client to kvs.
-				// 
 
-			// Handle when client wants to access the underlying database
-			case 
+		// Handle when client wants to access the underlying database
+		case dbRequest := <-kvs.dbRequestChannel:
+			if dbRequest.isGet {
+				valueBytes := get(dbRequest.key)
+				// fmt.Printf("key: %s\n", dbRequest.key)
+				// fmt.Printf("value: %s\n", string(valueBytes[:]))
 
-			//  Handle when client tries to disconnect
-			case
+				outputBytes := []byte(strings.Join([]string{dbRequest.key, string(valueBytes[:])}, ","))
+				for _, client := range(kvs.clients) {
+					fmt.Printf("Write <%v> to client\n", string(outputBytes))
+					client.bytesChannel <- outputBytes
+				}
+			} else {
+				put(dbRequest.key, []byte(dbRequest.value))
+			}
 
-			// Handle when client 		
+		// Handle when there is a new connection.
+		case connection := <-kvs.connectionChannel:
+			fmt.Println("Server got new connection ...")
+			client := Client{connection: connection,
+							bytesChannel: make(chan []byte),
+							quitReadChannel: make(chan bool),
+							quitWriteChannel: make(chan bool)}
+			kvs.clients = append(kvs.clients, client)
+			go read(kvs, &client)
+			go write(&client)
+
+		case deadClient := <-kvs.deadClient:
+			for i, client := range(kvs.clients) {
+				if client == deadClient {
+					kvs.clients = append(kvs.clients[:i], kvs.clients[i+1:]...)
+					break
+				}
+			}
+
+		//  Handle when client tries to disconnect
+		case countClientsRequest := <-kvs.countClientsRequestChannel:
+			countClientsRequest.countChannel <- len(kvs.clients)
+
+		case <-kvs.quitServer:
+			fmt.Println("Quiting server...")
+			for _, client := range(kvs.clients) {
+				client.connection.Close()
+				fmt.Println("Sending signals to quit reading and writing channels")
+				client.quitReadChannel <- true
+				client.quitWriteChannel <- true
+				fmt.Println("Read and write signals sent successfully")
+			}
+			fmt.Println("Quited the server")
+			return
+
+
+		// Handle when client disconnect
 		}
 	}
 }
 
-func listenToConnections(listener *Listener) {
+func listenToConnections(kvs *keyValueServer) {
 	for {
-		connection, err := listener.Accept()
-		if nil != err {
-			// handle err
-		} else {
-			// notify server handler thread that there is a new connection.
-			// Pass the new connection back through channel
+		connection, err := kvs.listener.Accept()
+		select {
+		case <-kvs.quitListening:
+			fmt.Println("Quit listenting")
+			return
+			
+		default:
+			if nil == err {
+				// notify server handler thread that there is a new connection.
+				// Pass the new connection back through channel.
+				kvs.connectionChannel <- connection
+				fmt.Println("Got new connections")
+			}
 		}
 	}
+}
+
+func read(kvs *keyValueServer, client *Client) {
+	// create reader from client.connection
+	for {
+		select {
+		case <-client.quitReadChannel:
+			fmt.Println("Quiting the read thread")
+			return
+		default:
+			bufReader := bufio.NewReader(client.connection)
+			buffer, err := bufReader.ReadBytes('\n')
+			if err != nil {
+				if err == io.EOF {
+					kvs.deadClient <- *client
+				} else {
+					return
+				}
+				fmt.Printf("Reading error: %v", err)
+			}
+			dbRequest := createDbRequest(buffer)
+			kvs.dbRequestChannel <- dbRequest
+		}
+	}
+}
+
+func write(client *Client) {
+	for {
+		select {
+		case <-client.quitWriteChannel:
+			fmt.Println("Quiting the write thread")
+			return
+		case bytes := <-client.bytesChannel:
+			client.connection.Write(bytes)
+		}
+	}
+}
+
+func createDbRequest(bytes []byte) DBRequest {
+	request := string(bytes[:])
+	fmt.Printf("Database request: %v\n", request)
+	trimmedRequest := strings.TrimSuffix(request, "\n")
+	tokens := strings.Split(trimmedRequest, ",")
+	// fmt.Printf("Tokens: %v \n", tokens)
+	
+	dbRequest := DBRequest{}
+	if tokens[0] == "get" {
+		dbRequest.isGet = true
+		dbRequest.key = tokens[1]
+	} else {
+		dbRequest.isGet = false
+		dbRequest.key = tokens[1]
+		dbRequest.value = tokens[2]
+	}
+	return dbRequest
 }
